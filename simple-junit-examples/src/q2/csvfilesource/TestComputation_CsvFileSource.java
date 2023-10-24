d6c63dd18e3c08ec1cfc01043ecc0beaae25ce55
noajurgens <noajurgens@unomaha.edu>
2023-10-24 17:03:07 -0500

    initial commit

---
+++ simple-junit-examples/src/q2/csvfilesource/TestComputation_CsvFileSource.java
@@ -0,0 +1,18 @@
+package q2.csvfilesource;
+
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class TestComputation_CsvFileSource {
+
+   // TODO: Write your answer to complete this test case
+   @ParameterizedTest
+   @CsvFileSource(resources = "computation_dataset.csv")
+   public void testGetDiscount(int id, int rate, int expected) throws Exception {
+	      
+	   Computation com = new Computation();
+	   int result = com.getDiscount(id, rate);
+	   Assert.assertEquals(expected, result);
+   }
+}
